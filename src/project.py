2a60487708fb81b5ad7b891ad6b56ca2e53fde56
moist <trieuhayden@gmail.com>
2026-05-08 23:48:30 -0500

    Added initial testing code to make sure pip install worked

---
+++ src/project.py
@@ -0,0 +1,20 @@
+import pyglet
+
+def main():
+    window = pyglet.window.Window()
+    label = pyglet.text.Label('Hello, world',
+                            font_name='Times New Roman',
+                            font_size=36,
+                            x=window.width//2, y=window.height//2,
+                            anchor_x='center', anchor_y='center')
+        
+    pyglet.app.run()
+
+    on_draw(window, label)
+
+def on_draw(window, label):
+    window.clear()
+    label.draw()
+
+if __name__ == "__main__":
+    main()
