d5b27788c072654c7f853c5e39249cf89a9390a7
moist <trieuhayden@gmail.com>
2026-05-11 22:30:02 -0500

    Updated pygame to display the hex and rgb values of the color clicked

--- src/project.py
+++ src/project.py
@@ -37,15 +37,21 @@ root.mainloop()
 
 def main():
     # Pygame Setup
-    pygame.init
+    pygame.init()
+    pygame.font.init()
     pygame.display.init()
     pygame.display.set_caption("Eyedropper Color picker")
-
     clock = pygame.time.Clock()
     dt = 0
     mouse_press = pygame.MOUSEBUTTONUP
+    font = pygame.font.Font(None, size=20)
+
+    # Color vars
     rgba_value = ()
     hex_value = ""
+    rgb_text = ""
+    hex_text = ""
+
     # Image Loader
     user_file = pygame.image.load(file_path)
     image_x = user_file.get_width() + 500
@@ -58,6 +64,13 @@ def main():
     screen.fill("Grey")
     screen.blit(user_file, dest=(50,50))
 
+    rgb_text = font.render(str(rgba_value),antialias=True, color="White")
+    screen.blit(rgb_text, ((screen.width/2) + 200, 50))
+
+    hex_text = font.render(hex_value,antialias=True, color="White")
+    screen.blit(hex_text, ((screen.width/2) + 200, screen.height/2))
+
+
     running = True
     while running:
         # Event Loop
@@ -68,10 +81,17 @@ def main():
                 rgba_value = rgba_value[0:3]
                 hex_value = rbg_to_hex(rgba_value)
 
+                rgb_text = font.render(str(rgba_value),
+                                       antialias=True, color="White")
+                hex_text = font.render(hex_value,
+                                       antialias=True, color="White")
+
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 running = False
 
         # Render & Display
+        screen.blit(rgb_text, ((screen.width/2) + 200, 50))
+        screen.blit(hex_text, ((screen.width/2) + 200, screen.height/2))
         pygame.display.flip()
         dt = clock.tick(24)
 
